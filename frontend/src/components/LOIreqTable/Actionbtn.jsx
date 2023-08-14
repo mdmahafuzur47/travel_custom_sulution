fff75a9af16ad62d5d81c739f5c716b2fdf2898f
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2023-08-14 16:47:04 +0600

    add

--- frontend/src/components/LOIreqTable/Actionbtn.jsx
+++ frontend/src/components/LOIreqTable/Actionbtn.jsx
@@ -5,10 +5,39 @@ import { BsThreeDots } from "react-icons/bs";
 import { FiSettings } from "react-icons/fi";
 import { AiOutlineShop } from "react-icons/ai";
 import { TiLightbulb } from "react-icons/ti";
+import Swal from "sweetalert2";
 
 function CardMenu(props) {
-  const { transparent } = props;
+  const { transparent,data,prop } = props;
   const [open, setOpen] = React.useState(false);
+
+  // Handle Approved
+  const handleApproved = () => {
+    let family=1;
+    const filterData = data.filter(data => data.reference === prop?.reference);
+    if (filterData.length > 1) {
+      family = filterData.length;
+    }
+
+    Swal.fire({
+      title: 'Are you sure to Approved?',
+      text: `Name: ${prop?.guest_name}, Passport Number: ${prop?.pasport_number}, Family: ${family}`,
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#422AFB',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Approved It!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Approved!',
+          '',
+          'success'
+        )
+      }
+    })
+  }
+
   return (
     <Dropdown
       button={
@@ -28,29 +57,19 @@ function CardMenu(props) {
       classNames={`${transparent ? "top-8" : "top-11"} right-0 w-max`}
       children={
         <div className="z-50 w-max rounded-xl bg-white py-3 px-4 text-sm shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-          <p className="hover:text-black flex cursor-pointer items-center gap-2 text-gray-600 hover:font-medium">
-            <span>
-              <AiOutlineUser />
-            </span>
-            Panel 1
+          <p onClick={handleApproved} className="hover:text-black flex cursor-pointer items-center gap-1 text-gray-600 hover:font-medium">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4L9.55 18Z"/></svg>
+            Approved
           </p>
           <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
             <span>
               <AiOutlineShop />
             </span>
-            Panel 2
+            Action
           </p>
-          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
-            <span>
-              <TiLightbulb />
-            </span>
-            Panel 3
-          </p>
-          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
-            <span>
-              <FiSettings />
-            </span>
-            Panel 4
+          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-1 pt-1 text-gray-600 hover:font-medium">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M1 21h22L12 2L1 21zm3.47-2L12 5.99L19.53 19H4.47zM11 16h2v2h-2zm0-6h2v4h-2z"/></svg>
+            Cancel
           </p>
         </div>
       }
--- frontend/src/components/LOIreqTable/REqu.jsx
+++ frontend/src/components/LOIreqTable/REqu.jsx
@@ -1,209 +1,285 @@
-import React, { useEffect, useState } from 'react'
-import Table from '../../views/public/Entry/ComplexTable';
-import Actionbtn from './Actionbtn';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Table from "../../views/public/Entry/ComplexTable";
+import Actionbtn from "./Actionbtn";
+import axios from "axios";
 const REqu = () => {
   const [datas, setDatas] = useState([]);
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await axios.get('url');
-        console.log(res);
+        const res = await axios.get("/api/loi/getall");
         setDatas(res.data);
       } catch (err) {
         console.log(err.message);
       }
-    }
+    };
     getData();
-    }, [])
+  }, []);
+
   return (
-    <div className='w-full relative'>
-        <div className='py-2'>
+    <div className="relative w-full">
+      <div className="py-2">
         <Table
-            columnsData={[
-              {
-                Header: "Guest Name",
-                accessor: "guestName",
+          columnsData={[
+            {
+              Header: "Guest Name",
+              accessor: "guestName",
+              Cell: (prop) => {
+                return (
+                  <div>
+                    <p>{prop.row.original?.guest_name}</p>
+                    <p>{prop.row.original?.pasport_number}</p>
+                  </div>
+                );
               },
-              {
-                Header: "Passport Number",
-                accessor: "passportNumber",
+            },
+            {
+              Header: "Travel Date",
+              accessor: "travelDate",
+              Cell: (prop) => {
+                return <p>{prop.row.original?.travel_date}</p>;
               },
-              {
-                Header: "Travel Date",
-                accessor: "travelDate",
+            },
+            {
+              Header: "Country Name",
+              accessor: "countryName",
+              Cell: (prop) => {
+                return <p>{prop.row.original?.country}</p>;
               },
-              {
-                Header: "Hotel Name",
-                accessor: "hotelName",
-              },
-              {
-                Header: "Passport copy",
-                accessor: "passportPhoto",
-                Cell: (prop) => {
-                  return prop.row.original.passportPhoto ? (
-                    <button
-                      title="visa copy available"
-                      className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-green-50 p-1 text-xl text-green-600"
-                    >
-                      {" "}
-                      <MaterialSymbolsDone />{" "}
-                    </button>
-                  ) : (
-                    <button
-                      title="visa copy not available"
-                      className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-red-50 p-1 text-xl text-red-600"
-                    >
-                      {" "}
-                      <IcTwotoneClose />{" "}
-                    </button>
-                  );
-                },
-              },
-              {
-                Header: "Visa copy",
-                accessor: "visaPhoto",
-                Cell: (prop) => {
-                  return prop.row.original.visaPhoto ? (
-                    <button
-                      title="visa copy available"
-                      className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-green-50 p-1 text-xl text-green-600"
-                    >
-                      {" "}
-                      <MaterialSymbolsDone />{" "}
-                    </button>
-                  ) : (
-                    <button
-                      title="visa copy not available"
-                      className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-red-50 p-1 text-xl text-red-600"
-                    >
-                      {" "}
-                      <IcTwotoneClose />{" "}
-                    </button>
-                  );
-                },
-              },
-              {
-                Header: "Action",
-                accessor: "action",
-                Cell: (prop) => {
-                  return (
-                    <div className='relative'>
-                        <Actionbtn/>
+            },
+            {
+              Header: "File",
+              accessor: "passportPhoto",
+              Cell: (prop) => {
+                return (
+                  <div>
+                    
+                    <div className="flex items-center gap-2">
+                      <span>
+                        {prop.row.original?.pasport_copy === ""  ? (
+                          <button
+                            title="visa copy available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10  bg-red-50 p-1 text-sm  text-red-600"
+                          >
+                            {" "}
+                            <IcTwotoneClose />{" "}
+                          </button>
+                        ) : (
+                          <button
+                            title="visa copy not available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10  p-1 text-sm bg-green-50 text-green-600"
+                          >
+                            {" "}
+                            <MaterialSymbolsDone />{" "}
+                          </button>
+                        )}
+                      </span>
+                      <p>password copy</p>
                     </div>
-                    // <button
-                    //   title="delete"
-                    //   onClick={() => {
-                    //     // deleteList(prop.row.original.id);
-                    //   }}
-                    //   className="rounded-full border-[1px] border-brand-600/10 bg-brand-50 p-1 text-xl text-red-600 hover:shadow-lg"
-                    // >
-                    //   {" "}
-                    //   <MaterialSymbolsDeleteOutline />{" "}
-                    // </button>
-                  );
-                },
+                    <div className="flex items-center gap-2">
+                      <span>
+                        {prop.row.original?.tiket_copy === ""  ? (
+                          <button
+                            title="visa copy available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10  bg-red-50 p-1 text-sm  text-red-600"
+                          >
+                            {" "}
+                            <IcTwotoneClose />{" "}
+                          </button>
+                        ) : (
+                          <button
+                            title="visa copy not available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10  p-1 text-sm bg-green-50 text-green-600"
+                          >
+                            {" "}
+                            <MaterialSymbolsDone />{" "}
+                          </button>
+                        )}
+                      </span>
+                      <p>ticket copy</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <span>
+                        {prop.row.original?.visa_copy ? (
+                          <button
+                            title="visa copy available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-green-50 p-1 text-sm text-green-600"
+                          >
+                            {" "}
+                            <MaterialSymbolsDone />{" "}
+                          </button>
+                        ) : (
+                          <button
+                            title="visa copy not available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-red-50 p-1 text-sm text-red-600"
+                          >
+                            {" "}
+                            <IcTwotoneClose />{" "}
+                          </button>
+                        )}
+                      </span>
+                      <p>visa copy</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <span>
+                        {prop.row.original?.hotel_copy ? (
+                          <button
+                            title="visa copy available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-green-50 p-1 text-sm text-green-600"
+                          >
+                            {" "}
+                            <MaterialSymbolsDone />{" "}
+                          </button>
+                        ) : (
+                          <button
+                            title="visa copy not available"
+                            className="cursor-cell rounded-full border-[1px] border-brand-600/10 bg-red-50 p-1 text-sm text-red-600"
+                          >
+                            {" "}
+                            <IcTwotoneClose />{" "}
+                          </button>
+                        )}
+                      </span>
+                      <p>hotel copy</p>
+                    </div>
+                  </div>
+                );
               },
-            ]}
-            tableData={[{
-                name:"nahid"
-            }]}
-          />
-        </div>
+            },
+            {
+              Header: "Agent Date",
+              accessor: "AgentDate",
+              // TODO: Add Agent in Database
+            },
+            {
+              Header: "Action",
+              accessor: "action",
+              Cell: (prop) => {
+                return (
+                  <div className="relative">
+                    <Actionbtn data={datas} prop={prop.row.original} />
+                  </div>
+                  // <button
+                  //   title="delete"
+                  //   onClick={() => {
+                  //     // deleteList(prop.row.original.id);
+                  //   }}
+                  //   className="rounded-full border-[1px] border-brand-600/10 bg-brand-50 p-1 text-xl text-red-600 hover:shadow-lg"
+                  // >
+                  //   {" "}
+                  //   <MaterialSymbolsDeleteOutline />{" "}
+                  // </button>
+                );
+              },
+            },
+          ]}
+          tableData={datas}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default REqu
-
+export default REqu;
 
 export function MaterialSymbolsAddNotesOutline(props) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v6.7q-.475-.225-.975-.388T19 11.075V5H5v14h6.05q.075.55.238 1.05t.387.95H5Zm0-3v1V5v6.075V11v7Zm2-1h4.075q.075-.525.238-1.025t.362-.975H7v2Zm0-4h6.1q.8-.75 1.788-1.25T17 11.075V11H7v2Zm0-4h10V7H7v2Zm11 14q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23Zm-.5-2h1v-2.5H21v-1h-2.5V15h-1v2.5H15v1h2.5V21Z"
-        ></path>
-      </svg>
-    );
-  }
-  
-  export function MaterialSymbolsDeleteOutline(props) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
-        ></path>
-      </svg>
-    );
-  }
-  
-  export function MaterialSymbolsDone(props) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4L9.55 18Z"
-        ></path>
-      </svg>
-    );
-  }
-  
-  export function IcTwotoneClose(props) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z"
-        ></path>
-      </svg>
-    );
-  }
-  
-  export function MaterialSymbolsAirplaneTicketOutlineRounded(props) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}
-      >
-        <path
-          fill="currentColor"
-          d="m11.8 12.9l-2.4.6l-1.075-.8q-.075-.05-.4-.1l-.125.05q-.225.05-.325.263t.025.412l1.15 2q.1.15.25.213t.325.012l8.525-2.25q.375-.1.563-.463t.087-.737q-.1-.375-.437-.562t-.713-.088l-2.45.65l-3.725-3.5q-.125-.125-.3-.162t-.35.012l-.125.025q-.35.075-.488.4t.038.625l1.95 3.4ZM4 20q-.825 0-1.413-.588T2 18v-3.375q0-.275.175-.475t.45-.25q.6-.2.988-.725T4 12q0-.65-.388-1.175t-.987-.725q-.275-.05-.45-.25T2 9.375V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm0-2h16V6H4v2.55q.925.55 1.463 1.463T6 12q0 1.075-.537 1.988T4 15.45V18Zm8-6Z"
-        ></path>
-      </svg>
-    );
-  }
-  
-  export function MaterialSymbolsArrowLeftAltRounded(props) {
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m7.85 13l2.85 2.85q.3.3.288.7t-.288.7q-.3.3-.712.313t-.713-.288L4.7 12.7q-.3-.3-.3-.7t.3-.7l4.575-4.575q.3-.3.713-.287t.712.312q.275.3.288.7t-.288.7L7.85 11H19q.425 0 .713.288T20 12q0 .425-.288.713T19 13H7.85Z"></path></svg>
-    )
-  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v6.7q-.475-.225-.975-.388T19 11.075V5H5v14h6.05q.075.55.238 1.05t.387.95H5Zm0-3v1V5v6.075V11v7Zm2-1h4.075q.075-.525.238-1.025t.362-.975H7v2Zm0-4h6.1q.8-.75 1.788-1.25T17 11.075V11H7v2Zm0-4h10V7H7v2Zm11 14q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23Zm-.5-2h1v-2.5H21v-1h-2.5V15h-1v2.5H15v1h2.5V21Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function MaterialSymbolsDeleteOutline(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function MaterialSymbolsDone(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4L9.55 18Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function IcTwotoneClose(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z"
+      ></path>
+    </svg>
+  );
+}
+
+export function MaterialSymbolsAirplaneTicketOutlineRounded(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m11.8 12.9l-2.4.6l-1.075-.8q-.075-.05-.4-.1l-.125.05q-.225.05-.325.263t.025.412l1.15 2q.1.15.25.213t.325.012l8.525-2.25q.375-.1.563-.463t.087-.737q-.1-.375-.437-.562t-.713-.088l-2.45.65l-3.725-3.5q-.125-.125-.3-.162t-.35.012l-.125.025q-.35.075-.488.4t.038.625l1.95 3.4ZM4 20q-.825 0-1.413-.588T2 18v-3.375q0-.275.175-.475t.45-.25q.6-.2.988-.725T4 12q0-.65-.388-1.175t-.987-.725q-.275-.05-.45-.25T2 9.375V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm0-2h16V6H4v2.55q.925.55 1.463 1.463T6 12q0 1.075-.537 1.988T4 15.45V18Zm8-6Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function MaterialSymbolsArrowLeftAltRounded(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m7.85 13l2.85 2.85q.3.3.288.7t-.288.7q-.3.3-.712.313t-.713-.288L4.7 12.7q-.3-.3-.3-.7t.3-.7l4.575-4.575q.3-.3.713-.287t.712.312q.275.3.288.7t-.288.7L7.85 11H19q.425 0 .713.288T20 12q0 .425-.288.713T19 13H7.85Z"
+      ></path>
+    </svg>
+  );
+}
