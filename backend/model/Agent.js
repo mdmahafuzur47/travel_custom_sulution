cfe153b3990d00d29d03063e4e8bb59a8b00a35f
al-imam <alimam01828@gmail.com>
2023-08-18 12:50:44 +0600

    implement login controller

--- backend/model/Agent.js
+++ backend/model/Agent.js
@@ -1,49 +1,53 @@
-const Schema = require('../database/schema');
-const Model = require('../database/Model');
+const Schema = require("../database/schema");
+const Model = require("../database/Model");
 
 const AgentSchema = new Schema({
-    username:{
-        type:'TEXT(255)',
-        req:true,
-    },
-    email:{
-        type:'TEXT(255)',
-        req:true,
-    },
-    phone:{
-        type:'TEXT(255)',
-        req:true,
-    },
-    photo:{
-        type:'TEXT(255)',
-        req:false
-    },
-    admin:{
-        type:'TEXT(255)',
-        req:false
-    },
-    password:{
-        type:'TEXT(255)',
-        req:true
-    },
-    status:{
-        type:"INT(255)",
-        req:true,
-    },
-    balence:{
-        type:'TEXT(255)',
-        req:true,
-    },
-    rate:{
-        type:'TEXT(255)',
-        req:true,
-    },
-    session:{
-        type:'TEXT(255)',
-        req:true
-    }
+  name: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  nid_no: {
+    type: "VARCHAR(25)",
+    req: true,
+  },
+  email: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  phone: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  photo: {
+    type: "TEXT(255)",
+    req: false,
+  },
+  admin: {
+    type: "TEXT(255)",
+    req: false,
+  },
+  password: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  status: {
+    type: "INT(255)",
+    req: true,
+  },
+  balance: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  rate: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  session: {
+    type: "TEXT(255)",
+    req: true,
+  },
 });
 
-const agent = new Model(AgentSchema,"agent")
+const agent = new Model(AgentSchema, "agent");
 
 module.exports = agent;
