d2da342d14e1d32090617a81cbe1680bd22b1f2c
al-imam <alimam01828@gmail.com>
2023-08-18 12:47:56 +0600

    create registration controller

---
+++ backend/controller/agent/registration.js
@@ -0,0 +1,34 @@
+const Agent = require("../../model/Agent");
+const bcript = require("bcrypt");
+
+const registration = async (req, res, next) => {
+  try {
+    const queryEmail = await Agent.findOne({ email: req.body.email });
+
+    console.log(queryEmail, "findOne query");
+
+    if (queryEmail.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "email already exist!", code: "registration" });
+    }
+
+    await Agent.Add({
+      name: req.body.username,
+      email: req.body.email,
+      nid_no: req.body.nid_no,
+      phone: req.body.phone,
+      password: "",
+      photo: "no-photo.png",
+      admin: "pending",
+      status: 0,
+      rate: "pending",
+    });
+
+    res.status(201).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = registration;
