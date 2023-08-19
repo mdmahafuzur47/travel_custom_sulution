26d73a7727eeafbdc55be6415b6d99acc1231e5b
al-imam <alimam01828@gmail.com>
2023-08-19 20:03:08 +0600

    create add-balance controller

---
+++ backend/controller/agent/addBalance.js
@@ -0,0 +1,19 @@
+const BalanceReq = require("../../model/BalanceReq");
+
+async function addBalance(req, res, next) {
+  try {
+    await BalanceReq.Add({
+      agent_id: req.body.agent_id,
+      transition_id: req.body.transition_id,
+      amount: req.body.amount,
+      message: req.body.message,
+    });
+
+    res.json({ success: true });
+  } catch (e) {
+    console.log("add-balance-agent", e);
+    next(e);
+  }
+}
+
+module.exports = addBalance;
