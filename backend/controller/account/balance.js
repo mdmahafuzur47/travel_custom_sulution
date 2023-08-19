eaa9c8858aa4987e2b566acb899bae5faa0684bd
al-imam <alimam01828@gmail.com>
2023-08-19 12:45:37 +0600

    create add-balance controller

--- backend/controller/account/balance.js
+++ backend/controller/account/balance.js
@@ -6,18 +6,29 @@ function getNum(str, fallback = 0) {
 }
 
 async function addBalance(req, res) {
-  const agent = await Agent.findById(req.body.id);
+  try {
+    const agent = await Agent.findById(req.body.id);
 
-  if (!agent) {
-    return res.status(404).json({
-      message: "agent not found!",
-      code: "404",
+    if (!agent) {
+      return res.status(404).json({
+        message: "agent not found!",
+        code: "404",
+      });
+    }
+
+    const balance = getNum(agent.balance) + getNum(req.body.balance);
+
+    const dbRes = await Agent.findByIdAndUpdate(req.body.id, {
+      balance,
     });
+
+    console.log(dbRes);
+
+    res.json({ success: true });
+  } catch (e) {
+    console.log("add-balance", e);
+    next(e);
   }
-
-  const balance = getNum(agent.balance) + getNum(req.body.balance);
-
-  const dbRes = await Agent.findByIdAndUpdate(req.body.id, {
-    balance,
-  });
 }
+
+module.exports = addBalance;
