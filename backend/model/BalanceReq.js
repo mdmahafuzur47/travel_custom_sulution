2866781f1d3ee7ebb653d9bfc64491fcb5e3ea8e
al-imam <alimam01828@gmail.com>
2023-08-19 20:02:11 +0600

    create requst balance model

---
+++ backend/model/BalanceReq.js
@@ -0,0 +1,22 @@
+const Schema = require("../database/schema");
+const Model = require("../database/Model");
+
+const AgentSchema = new Schema({
+  agent_id: {
+    type: "INT",
+    req: true,
+  },
+  amount: {
+    type: "INT",
+    req: true,
+  },
+  transition_id: {
+    type: "TEXT(255)",
+    req: true,
+  },
+  message: {
+    type: "TEXT(255)",
+  },
+});
+
+module.exports = new Model(AgentSchema, "BalanceReq");
