eaf9edf0c3e4d169b12bb7021af9ec916b15485d
al-imam <alimam01828@gmail.com>
2023-08-20 12:08:39 +0600

    create get-balance-requests controller

--- backend/controller/account/getBalanceRequests.js
+++ backend/controller/account/getBalanceRequests.js
@@ -1,1 +1,21 @@
-function getBalanceRequests(req, res, next) {}
+const BalanceRequests = require("../../model/BalanceReq");
+const Agent = require("../../model/Agent");
+
+async function getBalanceRequests(req, res, next) {
+  try {
+    const [balRes] = await BalanceRequests.findAll();
+    const [agtRes] = await Agent.findAll();
+
+    const requests = balRes.map((bal) => ({
+      ...bal,
+      agent: agtRes.find((agt) => agt.id === bal.id),
+    }));
+
+    res.json(requests);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+module.exports = getBalanceRequests;
