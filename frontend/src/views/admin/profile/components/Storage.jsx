1f9c3a9f7f53c7596b350a3af5b92cf0cfb6dbe1
al-imam <alimam01828@gmail.com>
2023-08-21 11:57:56 +0600

    conect storage api to frontend

--- frontend/src/views/admin/profile/components/Storage.jsx
+++ frontend/src/views/admin/profile/components/Storage.jsx
@@ -1,14 +1,24 @@
+import axios from "axios";
 import Card from "components/card";
-import CardMenu from "components/card/CardMenu";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsCloudCheck } from "react-icons/bs";
+
 const Storage = () => {
+  const [storageInfo, setStorageInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/api/admin/storage-info");
+        setStorageInfo(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <Card extra={"w-full h-full p-4"}>
-      <div className="ml-auto">
-        <CardMenu />
-      </div>
-      {/* Your storage */}
       <div className="mb-auto flex flex-col items-center justify-center">
         <div className="mt-2 flex items-center justify-center rounded-full bg-lightPrimary p-[26px] text-5xl font-bold text-brand-500 dark:!bg-navy-700 dark:text-white">
           <BsCloudCheck />
@@ -21,15 +31,20 @@ const Storage = () => {
         </p>
       </div>
 
-      {/* Progress bar */}
-
       <div className="flex flex-col">
         <div className="flex justify-between">
-          <p className="text-sm font-medium text-gray-600">25.6 GB</p>
-          <p className="text-sm font-medium text-gray-600">50 GB</p>
+          <p className="text-sm font-medium text-gray-600">
+            {storageInfo.usedStorageGB ?? 0} GB
+          </p>
+          <p className="text-sm font-medium text-gray-600">
+            {storageInfo.totalStorageGB ?? 0} GB
+          </p>
         </div>
         <div className="mt-2 flex h-3 w-full items-center rounded-full bg-lightPrimary dark:!bg-navy-700">
-          <span className="h-full w-1/2 rounded-full bg-brand-500 dark:!bg-white" />
+          <span
+            style={{ width: `${storageInfo.usedStoragePercentage ?? 0}%` }}
+            className="h-full rounded-full bg-brand-500 dark:!bg-white"
+          />
         </div>
       </div>
     </Card>
