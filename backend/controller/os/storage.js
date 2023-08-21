b082a270a287f26e41942c5ba9a2bc713f40c98a
al-imam <alimam01828@gmail.com>
2023-08-21 14:23:02 +0600

    fix: storage check

--- backend/controller/os/storage.js
+++ backend/controller/os/storage.js
@@ -1,22 +1,19 @@
-const os = require("os");
+const checkDiskSpace = require("check-disk-space").default;
 
 async function storage(req, res, next) {
   try {
-    const totalStorageBytes = os.totalmem();
-    const freeStorageBytes = os.freemem();
-    const usedStorage = totalStorageBytes - freeStorageBytes;
+    const disk = await checkDiskSpace("C:");
 
-    const totalStorageGB = parseInt((totalStorageBytes / 1024 ** 3) * 100);
-    const freeStorageGB = parseInt((freeStorageBytes / 1024 ** 3) * 100);
-    const usedStorageGB = parseInt((usedStorage / 1024 ** 3) * 100);
+    const totalStorage = disk.size;
+    const freeStorage = disk.free;
+    const usedStorage = totalStorage - freeStorage;
 
-    const freeStoragePercentage = parseInt(
-      (freeStorageBytes / totalStorageBytes) * 100
-    );
+    const totalStorageGB = parseInt(totalStorage / 1024 ** 3);
+    const freeStorageGB = parseInt(freeStorage / 1024 ** 3);
+    const usedStorageGB = parseInt(usedStorage / 1024 ** 3);
 
-    const usedStoragePercentage = parseInt(
-      (usedStorage / totalStorageBytes) * 100
-    );
+    const freeStoragePercentage = parseInt((freeStorage / totalStorage) * 100);
+    const usedStoragePercentage = parseInt((usedStorage / totalStorage) * 100);
 
     res.json({
       freeStoragePercentage,
