84872291f704f407f7301637a0cb5c602417311f
al-imam <alimam01828@gmail.com>
2023-08-21 12:53:31 +0600

    create controller fot cleaning cache

---
+++ backend/controller/os/clearCache.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+const pdfPath = path.join(__dirname, "../../GenaretePDF");
+const tempPath = path.join(__dirname, "../../upload/temp");
+
+console.log(pdfPath, tempPath);
+
+function clearCache(req, res, next) {
+  try {
+    deleteFilesInFolder(pdfPath, (name) => path.extname(name) === ".pdf");
+    deleteFilesInFolder(tempPath);
+
+    res.json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+}
+
+function deleteFilesInFolder(folderPath, check = () => true) {
+  const items = fs.readdirSync(folderPath);
+
+  items.forEach((item) => {
+    const itemPath = path.join(folderPath, item);
+    const stats = fs.statSync(itemPath);
+
+    if ((stats.isFile(), check(item))) {
+      fs.unlinkSync(itemPath);
+    }
+  });
+}
+
+module.exports = clearCache;
